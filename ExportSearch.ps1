c2bfb716df82302c1bfc534471357f17512bc3aa
Andrey1771 <Hohlov908@gmail.com>
2022-06-29 13:53:42 +0300

    Добавил скрипт для поиска названий файлов, содержащих export

---
+++ ExportSearch.ps1
@@ -0,0 +1,23 @@
+﻿# Путь до папки где будет происходить поиск
+$path =  "C:\LAST_FIX_SHARED_LIB\VisiologyDesigner\libs\dashboards\data-access-viewer-integration\src\lib"
+# Путь до файла, в который запишутся изменения
+$pathToInput = "C:\LAST_FIX_SHARED_LIB\VisiologyDesigner\libs\dashboards\data-access-viewer-integration\src\index.ts"
+# Паттерн поиска
+$patternOfSearch = "export{1}[A-Za-z0-9-_ ]+"
+
+
+# Содержимое папки, в которую будет производиться запись
+$data = (Get-Content -Encoding UTF8 -Path $pathToInput -raw)
+
+foreach ($file in $infolder)
+{
+    $result = Select-String $file -Pattern $patternOfSearch -Include "*.ts"
+
+    foreach ($textLine in $result)
+    {
+        $data += "`n" + $textLine.Line
+    }
+}
+
+# Устанавливаем новое значение содержимого файла записи
+Set-Content -Path $pathToInput -Value $data  
